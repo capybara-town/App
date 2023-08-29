5d2238948aec46081bcae579c8ceefa4d671ae82
김태윤 <k0278kim@gmail.com>
2023-08-29 14:35:55 +0900

    Apply .gitignore & login Screen

--- lib/src/screen/login/login_screen.dart
+++ lib/src/screen/login/login_screen.dart
@@ -13,80 +13,91 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  CupertinoPageScaffold(
+    return CupertinoPageScaffold(
       backgroundColor: ColorTheme.blackPoint,
       child: Stack(
         children: [
           ListView.builder(
             itemCount: 10,
-              itemBuilder: (BuildContext ctx, int idx) {
+              itemBuilder: (BuildContext context, int idx) {
                 return Container(
                   height: 186,
                   margin: const EdgeInsets.only(left: 32, right: 32, bottom: 25),
                   decoration: const BoxDecoration( color: ColorTheme.greyPoint),
                 );
               }),
-
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            height: 600,
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    ColorTheme.blackPoint.withOpacity(1),
-                    ColorTheme.blackPoint.withOpacity(1),
-                    ColorTheme.blackPoint.withOpacity(1),
-                    ColorTheme.blackPoint.withOpacity(0.9),
-                    ColorTheme.blackPoint.withOpacity(0.8),
-                    ColorTheme.blackPoint.withOpacity(0.5),
-                    ColorTheme.blackPoint.withOpacity(0),
-                  ],
-                ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  ColorTheme.blackPoint.withOpacity(0),
+                  ColorTheme.blackPoint.withOpacity(0),
+                  ColorTheme.blackPoint.withOpacity(0),
+                  ColorTheme.blackPoint.withOpacity(0),
+                  ColorTheme.blackPoint.withOpacity(0.3),
+                  ColorTheme.blackPoint.withOpacity(0.5),
+                  ColorTheme.blackPoint.withOpacity(0.8),
+                  ColorTheme.blackPoint.withOpacity(1),
+                  ColorTheme.blackPoint.withOpacity(1),
+                  ColorTheme.blackPoint.withOpacity(1),
+                  ColorTheme.blackPoint.withOpacity(1),
+                ],
               ),
             ),
           ),
          Positioned(
-          bottom: 245,
-          left: 103,
-          child: Row(
-              children: [
-                Text('카피바라', style: FontTheme.gradiantYellow),
-                const SizedBox(width: 5),
-                const Text('시작하기' ,style: FontTheme.headline1,)]),
-        ),
-          const Positioned(
-            bottom: 211,
-            left: 64,
-            child: Row(
-                children: [
-                  Text('만나보고 싶던 사람과 네트워킹을 해보세요', style: FontTheme.subtitle1GreyBold),
-          ]),),
-          Positioned(
-            width: 353, 
-            height: 53,
-            bottom: 100,
-            left: 21,
-            child: Bounce(
-                onTap: () => context.go('/frame'),
-                child: Container(
-                  decoration: BoxDecoration(color: const Color(0xffFEE500),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Row( children: [
-                    const SizedBox(width: 18),
-                    SizedBox(width: 20, child: SvgPicture.asset('asset/image/kakao-symbol.svg')),
-                    const SizedBox(width: 85),
-                    const Text('카카오로 시작하기', style: FontTheme.subtitle1BlackBold),
-                  ]),
+           left: 0,
+           right: 0,
+           bottom: 80,
+           child: Column(
+             children: [
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   Text('카피바라', style: FontTheme.gradientYellow),
+                   const SizedBox(width: 5),
+                   const Text('시작하기' ,style: FontTheme.headline1,),
+                 ]
+               ),
+               const SizedBox(height: 10),
+               const Text('만나보고 싶던 사람과 네트워킹을 해보세요', style: FontTheme.subtitle1GreyBold),
+               const SizedBox(height: 40),
+               kakaoButton(context)
+             ]
+           )
+         ),
+        ]
+      )
+    );
+  }
 
-                  ),
-                )
-              ),
-            ])
+  Widget kakaoButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: Bounce(
+        onTap: () => context.go('/frame'),
+        child: Container(
+          width: double.infinity,
+          height: 60,
+          decoration: BoxDecoration(color: const Color(0xffFEE500),
+              borderRadius: BorderRadius.circular(10)),
+          child: Stack(
+              children: [
+                Row(
+                  children: [
+                    const SizedBox(width: 20),
+                    SizedBox(width: 20, child: SvgPicture.asset('asset/image/kakao-symbol.svg')),
+                  ],
+                ),
+                const Center(child: Text('카카오로 시작하기', style: FontTheme.subtitle1BlackBold)),
+              ]
+          ),
+        ),
+      ),
     );
   }
 }
